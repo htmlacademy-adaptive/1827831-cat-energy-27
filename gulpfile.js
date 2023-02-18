c3628c8ca9f09e859f528397f1648b1b0529e7ef
Viktoriia <zalevskaya3103@mail.ru>
2023-02-18 16:41:12 +0100

    Добавляет шрифты

--- gulpfile.js
+++ gulpfile.js
@@ -2,24 +2,114 @@ import gulp from 'gulp';
 import plumber from 'gulp-plumber';
 import less from 'gulp-less';
 import postcss from 'gulp-postcss';
+import csso from 'postcss-csso';
+import rename from 'gulp-rename';
+import htmlmin from 'gulp-htmlmin';
+import terser from 'gulp-terser';
+import squoosh from 'gulp-libsquoosh';
+import svgo from 'gulp-svgmin';
+import svgstore from 'gulp-svgstore';
 import autoprefixer from 'autoprefixer';
 import browser from 'browser-sync';
+import {deleteSync} from 'del';
 
-// Styles
-
-export const styles = () => {
-  return gulp.src('source/less/style.less', { sourcemaps: true })
+// Build styles
+export const stylesBuild = () => {
+  return gulp.src('source/less/style.less', {sourcemaps: true})
     .pipe(plumber())
     .pipe(less())
     .pipe(postcss([
-      autoprefixer()
+      autoprefixer(),
+      csso()
     ]))
-    .pipe(gulp.dest('source/css', { sourcemaps: '.' }))
+    // .pipe(rename('style.min.css'))
+    .pipe(gulp.dest('source/css', {sourcemaps: false}))
     .pipe(browser.stream());
 }
 
-// Server
+// Copy minified styles
+export const copyMinifiedStyles = () => {
+  return gulp.src('source/css/style.css/style.min.css')
+    .pipe(gulp.dest('build/css'));
+}
 
+// HTML
+const html = () => {
+  return gulp.src('source/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build'));
+}
+
+// JS
+const js = () => {
+  return gulp.src('source/*.js')
+    .pipe(gulp.dest('build'));
+}
+
+// Scripts
+const scripts = () => {
+  return gulp.src('source/js/*.js')
+    .pipe(terser())
+    .pipe(gulp.dest('build/js'));
+}
+
+// Images
+const optimizeImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(squoosh())
+    .pipe(gulp.dest('build/img'))
+}
+
+const copyImages = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(gulp.dest('build/img'))
+}
+
+// WebP
+const createWebp = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(squoosh({
+      webp: {}
+    }))
+    .pipe(gulp.dest('build/img'))
+}
+
+// SVG
+const svg = () =>
+  gulp.src('source/img/*.svg')
+    .pipe(svgo())
+    .pipe(gulp.dest('build/img'));
+
+const sprite = () => {
+  return gulp.src('source/img/icons/*.svg')
+    .pipe(svgo({removeAttrs: {attrs: ['fill']}, plugins: [{removeViewBox: false,}]}))
+    .pipe(svgstore({
+      inlineSvg: true
+    }))
+    .pipe(rename('sprite.svg'))
+    .pipe(gulp.dest('build/img'));
+}
+
+// Copy
+const copy = (done) => {
+  gulp.src([
+    'source/fonts/*.{woff2,woff}',
+    'source/*.ico',
+    'source/*.webmanifest',
+  ], {
+    base: 'source'
+  })
+    .pipe(gulp.dest('build'))
+  done();
+}
+
+//Clean
+const clean = (done) => {
+  deleteSync('build');
+  done();
+};
+
+// Server
 const server = (done) => {
   browser.init({
     server: {
@@ -35,11 +125,53 @@ const server = (done) => {
 // Watcher
 
 const watcher = () => {
-  gulp.watch('source/less/**/*.less', gulp.series(styles));
-  gulp.watch('source/*.html').on('change', browser.reload);
+  gulp.watch('source/less/**/*.less', gulp.series(stylesBuild));
+  gulp.watch('source/*.html', gulp.series(html));
+  gulp.watch('source/js/*.js', gulp.series(js));
 }
 
+// Create WebP in source
+const prepareWebpInSource = () => {
+  return gulp.src('source/img/**/*.{jpg,png}')
+    .pipe(squoosh({
+      webp: {}
+    }))
+    .pipe(gulp.dest('source/img'))
+}
 
-export default gulp.series(
-  styles, server, watcher
+export const createWebpInSource = gulp.series(
+  prepareWebpInSource,
 );
+
+// Build
+export const build = gulp.series(
+  clean,
+  copy,
+  optimizeImages,
+  gulp.parallel(
+    stylesBuild,
+    html,
+    scripts,
+    svg,
+    sprite,
+    createWebp
+  ),
+);
+
+// Default
+export default gulp.series(
+  clean,
+  copy,
+  copyImages,
+  gulp.parallel(
+    stylesBuild,
+    html,
+    scripts,
+    svg,
+    sprite,
+    createWebp
+  ),
+  gulp.series(
+    server,
+    watcher
+  ));
